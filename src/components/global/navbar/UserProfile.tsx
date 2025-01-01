c14208dafbb22063750f7e918c3ac64877786243
NafiulHasan41 <nhashik40@gmail.com>
2025-01-02 05:39:07 +0600

    Edited the user dropdown and made the navbar Rsponsive layout

--- src/components/global/navbar/UserProfile.tsx
+++ src/components/global/navbar/UserProfile.tsx
@@ -7,14 +7,25 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
 import Image from "next/image";
+
 import { MdDashboard } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { IoLogOut } from "react-icons/io5";
+import { FaHome } from "react-icons/fa";
+import { SiFreelancer } from "react-icons/si";
+import { FcAbout } from "react-icons/fc";
+
+
 import { useContext } from "react";
 import { AuthContext } from "@/provider/AuthProvider";
+
 import useToast from "@/hooks/useToast";
+
 import axios from "axios";
+
+import Link from "next/link";
 
 
 const UserProfile = () => {
@@ -24,7 +35,7 @@ const UserProfile = () => {
       throw new Error("AuthContext must be used within an AuthProvider");
     }
   
-    const { logOut  } = authContext;
+    const { logOut , user  } = authContext;
     const showToast = useToast();
 
     const handleLogout = async () => {
@@ -51,12 +62,21 @@ const UserProfile = () => {
             <DropdownMenuTrigger>
                 {/* Trigger can include user profile picture and name */}
                 <div className="flex items-center space-x-2 cursor-pointer">
+                   
+                     <Image
+                        src="https://i.ibb.co.com/TtPv8g8/micro-service-logo.jpg" 
+                        alt="User Avatar"
+                        width={64}
+                        height={64}
+                        className="w-10 h-10 hidden md:block rounded-full"
+                    />
+                   
                     <Image
                         src="https://i.ibb.co.com/TtPv8g8/micro-service-logo.jpg" 
                         alt="User Avatar"
                         width={32}
                         height={32}
-                        className="w-8 h-8 rounded-full"
+                        className="w-8 h-8 md:hidden rounded-full"
                     />
                    
                 </div>
@@ -72,15 +92,44 @@ const UserProfile = () => {
                         className="w-8 h-8 rounded-full"
                     />
                     <div className="flex flex-col items-start">
-                        <span className="text-sm font-medium">Tom Smith</span>
-                        <span className="text-xs text-gray-500">user/admin</span>
+                        <span className="text-sm font-medium">{user?.name}</span>
+                        <span className="text-xs text-gray-500">{user?.role}</span>
                     </div>
                 </div>
              
                 <DropdownMenuSeparator />
                 {/* here I will write navigation for mobile device */}
                   
-                
+                <div className=" md:hidden" >
+                <DropdownMenuItem>
+                    <Link href="/">
+                    <div className="flex items-center space-x-2">
+                         <FaHome />
+                        <span>Home</span>
+                    </div>
+                    </Link>
+                   
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link href="/jobs">
+                    <div className="flex items-center space-x-2">
+                         <SiFreelancer />
+                        <span>Browse Job</span>
+                    </div>
+                    </Link>
+                   
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link href="/about_us">
+                    <div className="flex items-center space-x-2">
+                         <FcAbout />
+                        <span>About us</span>
+                    </div>
+                    </Link>
+                   
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                </div>
                 
                 {/* Menu Items */}
                 <DropdownMenuItem>
